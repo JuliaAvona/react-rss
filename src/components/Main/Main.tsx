6e94b2309a6f362709a02c30c0d0ac85b02c11c3
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-12-02 20:19:17 -0600

    feat: added styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import './Main.css';
 
 const Main = () => {
     return (
-        <>
+        <div className="main">
             <Navbar></Navbar>
             <main>Main Page</main>
-        </>
+        </div>
     );
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/form1">Form 1</Link>
-            <Link to="/form2">Form 2</Link>
+            <Link to="/form1"><button>Form 1</button></Link>
+            <Link to="/form2"><button>Form 2</button></Link>
         </nav>
     );
 };
